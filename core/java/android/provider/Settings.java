762e5b30a3fb058c9579ad967526ba18398598f3
blunden <blunden2@gmail.com>
2012-07-30 15:31:21 +0200

    Framework: Add option to always show battery status on lockscreen (1/2)
    Based on the feature by burnsra in CM7
    
    Authored by Blunden
    Tweaked by DvTonder
    
    Screenshot: https://dl.dropbox.com/u/50993944/Screenshot_2012-08-03-08-47-10.png
    
    Patchset 1-8: Initial code
    Patchset 9  : Minor tweaks
    
    Change-Id: I642da4dcd82a776a74d77031d37acc76f97b25af
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
    	policy/src/com/android/internal/policy/impl/KeyguardUpdateMonitor.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2937,6 +2937,11 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_BACKGROUND = "lockscreen_background";
 
+        /**
+          * Always show the battery status on the lockscreen
+          * @hide
+          */
+        public static final String LOCKSCREEN_ALWAYS_SHOW_BATTERY = "lockscreen_always_show_battery";
 
         /**
          * Show the pending notification counts as overlays on the status bar
@@ -3238,6 +3243,7 @@ public final class Settings {
             POWER_MENU_REBOOT_ENABLED,
             POWER_MENU_AIRPLANE_ENABLED,
             POWER_MENU_SILENT_ENABLED,
+            LOCKSCREEN_ALWAYS_SHOW_BATTERY,
         };
 
         // Settings moved to Settings.Secure
