aded2515e3e70437f64166e488603f05c9c17494
Robert Burns <burnsra@gmail.com>
2013-01-09 18:03:46 -0500

    Add Quick Settings USB Tether tile (2 of 2)
    Patch Set 8: Make tile dynamic
    
    - Thanks to Björn Lundén for the drawables
    
    Change-Id: I414848f6cb8a789d0b5edfcb79ab53cb85ebc20e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -28,6 +28,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.ContentObserver;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.provider.Settings;
@@ -60,6 +61,7 @@ import com.android.systemui.quicksettings.UserTile;
 import com.android.systemui.quicksettings.WiFiDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
+import com.android.systemui.quicksettings.UsbTetherTile;
 
 public class QuickSettingsController {
     private static String TAG = "QuickSettingsController";
@@ -99,6 +101,7 @@ public class QuickSettingsController {
     public static final String TILE_LTE = "toggleLte";
     public static final String TILE_WIMAX = "toggleWimax";
     public static final String TILE_NFC = "toggleNfc";
+    public static final String TILE_USBTETHER = "toggleUsbTether";
 
     private static final String TILE_DELIMITER = "|";
     private static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -150,6 +153,7 @@ public class QuickSettingsController {
     public static final int SYNC_TILE = 19;
     public static final int NFC_TILE = 20;
     public static final int SCREENTIMEOUT_TILE = 21;
+    public static final int USBTETHER_TILE = 22;
     public static final int USER_TILE = 99;
     private InputMethodTile IMETile;
 
@@ -261,6 +265,9 @@ public class QuickSettingsController {
         if (Settings.System.getInt(resolver, Settings.System.QS_DYNAMIC_IME, 1) == 1) {
             mQuickSettings.add(IME_TILE);
         }
+        if (deviceSupportsUsbTether() && Settings.System.getInt(resolver, Settings.System.QS_DYNAMIC_USBTETHER, 1) == 1) {
+            mQuickSettings.add(USBTETHER_TILE);
+        }
     }
 
     private void setupQuickSettings() {
@@ -353,6 +360,11 @@ public class QuickSettingsController {
         return (BluetoothAdapter.getDefaultAdapter() != null);
     }
 
+    boolean deviceSupportsUsbTether() {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return (cm.getTetherableUsbRegexs().length != 0);
+    }
+
     void setBar(PanelBar bar) {
         mBar = bar;
     }
@@ -434,6 +446,9 @@ public class QuickSettingsController {
                 break;
             case SCREENTIMEOUT_TILE:
                 qs = new ScreenTimeoutTile(mContext, inflater, mContainerView, this);
+                break;
+            case USBTETHER_TILE:
+                qs = new UsbTetherTile(mContext, inflater, mContainerView, this);
                 break;
             }
             if (qs != null) {
