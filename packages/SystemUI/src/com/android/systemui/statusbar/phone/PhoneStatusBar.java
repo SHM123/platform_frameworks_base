e8945751ba6b33b7418be1679c60ef6a4d0612ec
Thiago Vinícius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2013-01-22 18:11:56 -0300

    Quick Settings: Fix flip-to-settings animation
    Bug is reproduced as follows:
    
    1. close the notification area while Quick Settings is showing
    2. open the notification area, so that the notifications show
    3. click the button on the top, to switch to quick settings.
    
    Notice that only half of the animation is played. Having some
    notifications shown helps visualizing it.
    
    This bug was introduced by
    
    I3cab45f72fe3c5fa40d4631fc6a84ea7bfe51bd1
    http://review.cyanogenmod.org/29118
    (Yes, it was me. Sorry.)
    
    Change-Id: I31510617bf7e78fa09305a9c323b6ce30c058218

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1603,6 +1603,11 @@ public class PhoneStatusBar extends BaseStatusBar {
         final boolean halfWayDone = mScrollView.getVisibility() == View.VISIBLE;
         final int zeroOutDelays = halfWayDone ? 0 : 1;
 
+        if (!halfWayDone) {
+            mScrollView.setScaleX(0f);
+            mFlipSettingsView.setScaleX(1f);
+        }
+
         // Only show the Power widget if it should be shown
         mPowerWidget.updateVisibility();
 
@@ -1724,6 +1729,11 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         final boolean halfWayDone = mFlipSettingsView.getVisibility() == View.VISIBLE;
         final int zeroOutDelays = halfWayDone ? 0 : 1;
+
+        if (!halfWayDone) {
+            mFlipSettingsView.setScaleX(0f);
+            mScrollView.setScaleX(1f);
+        }
 
         mFlipSettingsView.setVisibility(View.VISIBLE);
         mFlipSettingsViewAnim = start(
